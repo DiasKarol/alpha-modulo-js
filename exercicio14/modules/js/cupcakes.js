3c57d8e3b29c7a63f0231462b04880788b4c8a1e
DiasKarol <karol.gulbarg@gmail.com>
2022-07-03 22:10:47 -0300

    spa - doceria

---
+++ exercicio14/modules/js/cupcakes.js
@@ -0,0 +1,15 @@
+export default () => {
+    const conteiner = document.createElement('section');
+
+    const conteudo = `    
+    <img id="imgCupcake" src="./modules/imagens/cupcake.jpg" alt="um mini bolo com cobertura contendo uns pedacinhos de biscoito misturado e meio biscoito oreo">
+    <div id="divCupcake">
+        <h2>Cupcakes</h2>
+        <p>Parece que eles vieram mesmo para ficar. Os cupcakes conquistaram seu espaço em muitas mesas brasileiras de doces. E aqui na SPA.JS não é diferente! esse bolinho individual é otimo para comemorar datas importantes, mas também é uma maravilha para se deliciar em quelquer dia da semana. E para atender a todos os gostos, nossa variedade de sabores é de deixar agua na boca, venha conferir!</p>
+        <p>Atendemos toda a região!</p>
+    </div>
+    `;
+
+    conteiner.innerHTML = conteudo;
+    return conteiner;
+}
