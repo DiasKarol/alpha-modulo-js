66bb6a5a83c56cccd4ecbb92df3d5866f403e4c1
DiasKarol <karol.gulbarg@gmail.com>
2022-07-03 21:59:35 -0300

    Spa - doceria

---
+++ exercicio14/exercicio14 - versao pedida/modules/js/doces.js
@@ -0,0 +1,17 @@
+export default () => {
+    const conteiner = document.createElement('section');
+
+    const conteudo = `
+    <img id="imgDoces" src="./modules/imagens/doces.jpg" alt="imagens de varios donuts bem coloridos">
+    <div id="divDoces">
+        <h2>Doces</h2>
+        <p>Que o seu dia seja doce como o algodão, alegre como um parque de diversão e com o sabor que pede o seu coração!</p>
+        <p>Temos os mais diversos doces, entre em contato para saber mais sobre o doce que seu coração esta pedindo!</p>
+        <p>Nosso telefone: <b>(21) 9-9999-9999</b></p>
+        <p>Atendemos toda a região!</p>
+    </div>
+    `;
+
+    conteiner.innerHTML = conteudo;
+    return conteiner;
+}
